417e87ecbdc0d0872946e38e4cb819362ffa268a
Kunal2806 <kunalgoswami.2806@gmail.com|>
2024-10-18 00:09:30 +0530

    some chages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 // import LoadingPage from "./componenta/LoadingPage";
-import "./App.css";
 import Cart from "./componenta/Cart";
 
 import WelcomePage from "./componenta/WelcomePage";
@@ -7,7 +6,7 @@ import WelcomePage from "./componenta/WelcomePage";
 import HomePage from "./componenta/HomePage";
 
 import { Routes, Route } from "react-router-dom";
-
+import "./App.css";
 function App() {
   return (
     <>
